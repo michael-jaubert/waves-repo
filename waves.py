ca0a37046aebf6e0ea02c2d0b7dc945e4bf27205
michael-jaubert <michael.jaubert@gmail.com>
2022-06-23 15:13:57 -1000

    Updating waves.py

--- waves.py
+++ waves.py
@@ -13,7 +13,7 @@ import pytz
 #    os.remove('51101.spec'):
 
 # This regex will split the data file into usable pieces, see "Regex Group Key" below
-wave_data_regex = re.compile(r'(\d{4})\s(\d{2})\s(\d{2})\s(\d{2})\s(\d{2})\s+(\d+\.\d)\s+(\d+\.\d)\s+(\d+\.\d)\s+\d+\.\d\s+\d+\.\d\s+(\S+)\s+(\S+)\s+\S+\s+\d+\.\d+\s+(\d+)')
+wave_data_regex = re.compile(r'(\d{4})\s(\d{2})\s(\d{2})\s(\d{2})\s(\d{2})\s+(\d+\.\d)\s+(\d+\.\d)\s+(\d+\.\d)(.+)')
 """     Regex Group Key:
         group(1) = year
         group(2) = month
@@ -23,8 +23,7 @@ wave_data_regex = re.compile(r'(\d{4})\s(\d{2})\s(\d{2})\s(\d{2})\s(\d{2})\s+(\d
         group(6) = WVHT (the average height (meters) of the highest one-third of waves)
         group(7) = SwH (swell height, vertical distance (meters) between swell crest and successding trough)
         group(8) = SwP (period, or time it takes successive swell wave crests to pass a fixed point)
-        group(9) = SwD (The direction (NWSE) from which the swell waves at the swell wave period (SWPD) are coming)
-        group(10) = MWD (The direction (in degrees) from which the waves at the dominant period (DPD) are coming) 
+        group(9) = Rest of string
 """
 # This regex will split the local_time string, in order to 
 local_time_regex = re.compile(r'(\d+)-(\d+)-(\d+)\s(\d+):(\d+)')
@@ -62,6 +61,8 @@ def convert_to_hst_and_add_travel_time(datetime_string, period_string):
 
 def convert_meters_to_feet(height_in_meters):
     height_in_feet = height_in_meters * 3.28084
+    height_in_feet = round(height_in_feet, 1)
+    return str(height_in_feet)
 
 for i in range(26):
     line = raw_data.readline().strip('\n')
@@ -70,11 +71,15 @@ for i in range(26):
         print(line)
     else:
         # Print out the rest of the wave data with date/time of when it will hit Waimea Bay Buoy
+        print(line)
         mo = wave_data_regex.search(line)
         utc_time_string = ('%s %s %s %s %s 00' % (mo.group(1), mo.group(2), mo.group(3), mo.group(4), mo.group(5)))
         period = mo.group(8)
         local_time = convert_to_hst_and_add_travel_time(utc_time_string, period)
         local_time = local_time.replace('-', ' ')
-        print(local_time)
+        WVHT = convert_meters_to_feet(float(mo.group(6)))
+        SwH = convert_meters_to_feet(float(mo.group(7)))
+        rest_of_string = mo.group(9)
+        print(local_time + '  ' + WVHT + '  ' + SwH + ' ' + period + rest_of_string)
         
 #os.remove('51101.spec')
